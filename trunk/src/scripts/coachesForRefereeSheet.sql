bf7647ab466339437a101d100844c1149fe65f4a
David Giannini <dag@pinger.com>
2018-09-14 17:28:46 -0700

    referee scripts and chasePalmPark image

---
+++ trunk/src/scripts/coachesForRefereeSheet.sql
@@ -0,0 +1,18 @@
+select
+    concat(case when d.gender = 'Boys' then 'B' else 'G' end, d.name) as Division,
+    right(t.nameId, (length(t.nameId) - position("-" in t.nameId))) as 'Team#',
+    t.nameId as TeamID,
+    c.name as Coach
+from
+    season as s
+    join division as d on
+        d.seasonId = s.id
+        and d.scoringTracked = 1
+        and d.name not in ("16U", "18U")
+    join team as t on
+        t.divisionId = d.id
+    join coach as c on
+        c.teamId = t.id
+where
+    s.name = '2018 - League'
+order by 1, 2;
--- trunk/src/scripts/refereePoints.sql
+++ trunk/src/scripts/refereePoints.sql
@@ -21,6 +21,53 @@ load data infile "/tmp/refereeCounts.csv"
     lines terminated by '\r'
     ignore 1 lines;
 
+drop table if exists rawRefereesByTeam;
+create table rawRefereesByTeam
+(
+    division      varchar(20),
+    teamNumber    int,
+    teamId        varchar(20),
+    coachName     varchar(64),
+    refereeName1  varchar(64),
+    refereeName2  varchar(64),
+    refereeName3  varchar(64),
+    refereeName4  varchar(64),
+    refereeName5  varchar(64),
+    refereeName6  varchar(64),
+    refereeName7  varchar(64),
+    refereeName8  varchar(64),
+    refereeName9  varchar(64),
+    refereeName10 varchar(64),
+    
+    unique key ux_ref1 (teamId, refereeName1),
+    unique key ux_ref2 (teamId, refereeName2),
+    unique key ux_ref3 (teamId, refereeName3),
+    unique key ux_ref4 (teamId, refereeName4),
+    unique key ux_ref5 (teamId, refereeName5),
+    unique key ux_ref6 (teamId, refereeName6),
+    unique key ux_ref7 (teamId, refereeName7),
+    unique key ux_ref8 (teamId, refereeName8),
+    unique key ux_ref9 (teamId, refereeName9),
+    unique key ux_ref10 (teamId, refereeName10),
+    
+    index ix_refereeName1 (refereeName1),
+    index ix_refereeName2 (refereeName2),
+    index ix_refereeName3 (refereeName3),
+    index ix_refereeName4 (refereeName4),
+    index ix_refereeName5 (refereeName5),
+    index ix_refereeName6 (refereeName6),
+    index ix_refereeName7 (refereeName7),
+    index ix_refereeName8 (refereeName8),
+    index ix_refereeName9 (refereeName9),
+    index ix_refereeName10 (refereeName10)
+);
+
+load data infile "/tmp/refereesByTeam.csv"
+    into table rawRefereesByTeam
+    fields terminated by ','
+    lines terminated by '\r'
+    ignore 1 lines;
+
 drop table if exists team;
 create table team
 (
@@ -28,17 +75,42 @@ create table team
     teamNumber  int,
     teamId      varchar(20),
     coachName   varchar(64),
-    refereeName varchar(64),
     
-    primary key (teamId, refereeName),
-    index ix_refereeName (refereeName)
+    primary key (teamId)
 );
 
-load data infile "/tmp/refereesByTeam.csv"
-    into table team
-    fields terminated by ','
-    lines terminated by '\r'
-    ignore 1 lines;
+insert into team
+    select division, teamNumber, teamId, coachName from rawRefereesByTeam;
+
+drop table if exists refereeByTeam;
+create table refereeByTeam
+(
+    teamId      varchar(20),
+    refereeName varchar(64),
+    
+    primary key (teamId, refereeName)
+);
+
+insert into refereeByTeam
+    select teamId, refereeName1 from rawRefereesByTeam where refereeName1 != '';
+insert into refereeByTeam
+    select teamId, refereeName2 from rawRefereesByTeam where refereeName2 != '';
+insert into refereeByTeam
+    select teamId, refereeName3 from rawRefereesByTeam where refereeName3 != '';
+insert into refereeByTeam
+    select teamId, refereeName4 from rawRefereesByTeam where refereeName4 != '';
+insert into refereeByTeam
+    select teamId, refereeName5 from rawRefereesByTeam where refereeName5 != '';
+insert into refereeByTeam
+    select teamId, refereeName6 from rawRefereesByTeam where refereeName6 != '';
+insert into refereeByTeam
+    select teamId, refereeName7 from rawRefereesByTeam where refereeName7 != '';
+insert into refereeByTeam
+    select teamId, refereeName8 from rawRefereesByTeam where refereeName8 != '';
+insert into refereeByTeam
+    select teamId, refereeName9 from rawRefereesByTeam where refereeName9 != '';
+insert into refereeByTeam
+    select teamId, refereeName10 from rawRefereesByTeam where refereeName10 != '';
 
 drop table if exists refereeTeamCount;
 create table refereeTeamCount
@@ -54,9 +126,7 @@ insert into refereeTeamCount (refereeName, teamCount)
         refereeName,
         count(1) as teamCount
     from
-        team
-    where
-        refereeName != ''
+        refereeByTeam
     group by
         1;
 -- select * from refereeTeamCount order by teamCount desc;
@@ -76,8 +146,6 @@ insert into refereeGameCount (refereeName, games)
         sum(count) as games
     from
         game
-    where
-        refereeName != ''
     group by
         1;
 -- select * from refereeGameCount order by gameCount desc;
@@ -92,282 +160,96 @@ create table gamesByTeam
     primary key (teamId, gameDate)
 );
 
+-- game:             for each referee, number of games ref'd that day and maxGamesAllowed
+-- team:             basic team info
+-- refereeByTeam:    Team's assinged referees
+-- refereeTeamCount: Count of teams referee is helping
+--
+-- Rule: Team can earn at most maxGamesPerDay
+-- Rule: Referee can work many games, games are divided by number of teams they support
+
+-- Referee'd games counted for team on given day
 insert into gamesByTeam (teamId, gameDate, gameCount)
     select
-        g.teamId,
-        g.day,
-        sum(round(g.gameCount / r.teamCount, 1)) as gameCount
+        data.teamId,
+        data.day,
+        case when data.maxGamesPerTeam > data.gameCount then data.gameCount else data.maxGamesPerTeam end as gameCount
     from
         (
+            -- Games per day for team and capture maxGamesPerTeam allowed for that day
             select
-                t.teamId,
-                g.day,
-                t.refereeName,
-                sum(g.count) as gameCount
+                refereesByTeamByDay.teamId,
+                refereesByTeamByDay.day,
+                max(refereesByTeamByDay.maxGamesPerTeam) as maxGamesPerTeam,
+                sum(refereesByTeamByDay.gamesForTeam) as gameCount
             from
-                team as t
-                join game as g on
-                    g.refereeName = t.refereeName
+                (
+                    -- Games for team by day, by referee, with percent split for refs helping multiple teams
+                    select
+                        t.teamId,
+                        g.day,
+                        g.maxGamesPerTeam,
+                        r.refereeName,
+                        round(g.count / c.teamCount, 1) as gamesForTeam
+                    from
+                        team as t
+                        join refereeByTeam as r on
+                            r.teamId = t.teamId
+                        join game as g on
+                            g.refereeName = r.refereeName
+                        join refereeTeamCount as c on
+                            c.refereeName = r.refereeName
+                ) as refereesByTeamByDay
             group by
-                1, 2, 3
-        ) as g
-        join refereeTeamCount as r on
-            r.refereeName = g.refereeName
-    group by
-        1, 2;
+                1, 2
+        ) as data;
 
--- select * from gamesByTeam order by gameCount desc;
--- select * from gamesByTeam order by teamId desc;
-
-drop table if exists teamRefs;
-create table teamRefs
-(
-    teamId      varchar(20),
-    coachName   varchar(64),
-    ref1        varchar(64) default '',
-    ref2        varchar(64) default '',
-    ref3        varchar(64) default '',
-    ref4        varchar(64) default '',
-    ref5        varchar(64) default '',
-    ref6        varchar(64) default '',
-    ref7        varchar(64) default '',
-    ref8        varchar(64) default '',
-    ref9        varchar(64) default '',
-    ref10       varchar(64) default '',
-    
-    primary key (teamId, coachName)
-);
-
-insert ignore into teamRefs (teamId, coachName, ref1)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team;
-
-insert into teamRefs (teamId, coachName, ref2)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId and r.ref1 = t.refereeName
-        )
-on duplicate key
-    update ref2 = t.refereeName;
-
-insert into teamRefs (teamId, coachName, ref3)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId
-                and (r.ref1 = t.refereeName
-                or r.ref2 = t.refereeName)
-        )
-on duplicate key
-    update ref3 = t.refereeName;
-
-insert into teamRefs (teamId, coachName, ref4)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId
-                and (r.ref1 = t.refereeName
-                or r.ref2 = t.refereeName
-                or r.ref3 = t.refereeName)
-        )
-on duplicate key
-    update ref4 = t.refereeName;
-
-insert into teamRefs (teamId, coachName, ref5)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId
-                and (r.ref1 = t.refereeName
-                or r.ref2 = t.refereeName
-                or r.ref3 = t.refereeName
-                or r.ref4 = t.refereeName)
-        )
-on duplicate key
-    update ref5 = t.refereeName;
-
-insert into teamRefs (teamId, coachName, ref6)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId
-                and (r.ref1 = t.refereeName
-                or r.ref2 = t.refereeName
-                or r.ref3 = t.refereeName
-                or r.ref4 = t.refereeName
-                or r.ref5 = t.refereeName)
-        )
-on duplicate key
-    update ref6 = t.refereeName;
-
-insert into teamRefs (teamId, coachName, ref7)
-    select
-        teamId,
-        coachName,
-        refereeName
-    from
-        team as t
-    where
-        not exists (
-            select * from teamRefs as r where r.teamId = t.teamId
-                and (r.ref1 = t.refereeName
-                or r.ref2 = t.refereeName
-                or r.ref3 = t.refereeName
-                or r.ref4 = t.refereeName
-                or r.ref5 = t.refereeName
-                or r.ref6 = t.refereeName)
-        )
-on duplicate key
-    update ref7 = t.refereeName;
 
 /* Games for Team */
 select
     t.teamId,
     t.coachName,
-    case when t.ref1 = '' then '' else concat(t.ref1, ": ", round(ifnull(r1.games,0) / ifnull(c1.teamCount, 1), 1)) end as 'ref1:games',
-    case when t.ref2 = '' then '' else concat(t.ref2, ": ", round(ifnull(r2.games,0) / ifnull(c2.teamCount, 1), 1)) end as 'ref2:games',
-    case when t.ref3 = '' then '' else concat(t.ref3, ": ", round(ifnull(r3.games,0) / ifnull(c3.teamCount, 1), 1)) end as 'ref3:games',
-    case when t.ref4 = '' then '' else concat(t.ref4, ": ", round(ifnull(r4.games,0) / ifnull(c4.teamCount, 1), 1)) end as 'ref4:games',
-    case when t.ref5 = '' then '' else concat(t.ref5, ": ", round(ifnull(r5.games,0) / ifnull(c5.teamCount, 1), 1)) end as 'ref5:games',
-    case when t.ref6 = '' then '' else concat(t.ref6, ": ", round(ifnull(r6.games,0) / ifnull(c6.teamCount, 1), 1)) end as 'ref6:games',
-    round(ifnull(r1.games,0) / ifnull(c1.teamCount, 1), 1)
-        + round(ifnull(r2.games,0) / ifnull(c2.teamCount, 1), 1)
-        + round(ifnull(r3.games,0) / ifnull(c3.teamCount, 1), 1)
-        + round(ifnull(r4.games,0) / ifnull(c4.teamCount, 1), 1)
-        + round(ifnull(r5.games,0) / ifnull(c5.teamCount, 1), 1)
-        + round(ifnull(r6.games,0) / ifnull(c6.teamCount, 1), 1) as totalRefGames
+    sum(ifnull(gameCount, 0)) as totalGamesRefereed,
+    case when 20 - sum(ifnull(gameCount, 0)) > 0 then
+        20 - sum(ifnull(gameCount, 0)) else 0 end as gamesNeededToQualifyForTournament
 from
-    teamRefs as t
-    left outer join refereeGameCount as r1 on
-        r1.refereeName = t.ref1
-    left outer join refereeTeamCount as c1 on
-        c1.refereeName = t.ref1
-    left outer join refereeGameCount as r2 on
-        r2.refereeName = t.ref2
-    left outer join refereeTeamCount as c2 on
-        c2.refereeName = t.ref2
-    left outer join refereeGameCount as r3 on
-        r3.refereeName = t.ref3
-    left outer join refereeTeamCount as c3 on
-        c3.refereeName = t.ref3
-    left outer join refereeGameCount as r4 on
-        r4.refereeName = t.ref4
-    left outer join refereeTeamCount as c4 on
-        c4.refereeName = t.ref4
-    left outer join refereeGameCount as r5 on
-        r5.refereeName = t.ref5
-    left outer join refereeTeamCount as c5 on
-        c5.refereeName = t.ref5
-    left outer join refereeGameCount as r6 on
-        r6.refereeName = t.ref6
-    left outer join refereeTeamCount as c6 on
-        c6.refereeName = t.ref6
+    team as t
+    left outer join gamesByTeam as g on
+        g.teamId = t.teamId
+group by
+    1, 2
 order by
     1;
 
 /* Games By Day for Team */
 select
-    teamId,
-    coachName,
-    week1Total,
-    week2Total,
-    week3Total,
-    week4Total,
-    week5Total,
-    week6Total,
-    week7Total,
-    week8Total,
-    week1Total + week2Total + week3Total + week4Total + week5Total + week6Total + week7Total + week8Total as totalRefGames,
-    week1VAP,
-    week2VAP,
-    week3VAP,
-    week4VAP,
-    week5VAP,
-    week6VAP,
-    week7VAP,
-    week8VAP,
-    week1VAP + week2VAP + week3VAP + week4VAP as firstHalfVAPRefGames,
-    week5VAP + week6VAP + week7VAP + week8VAP as secondHalfVAPRefGames,
-    week1VAP + week2VAP + week3VAP + week4VAP + week5VAP + week6VAP + week7VAP + week8VAP as totalVAPRefGames
+    t.teamId,
+    t.coachName,
+    gDate.gameDate as gameWeekend,
+    sum(ifnull(g.gameCount, 0)) as gamesRefereed
 from
-(
-    select
-        t.teamId,
-        t.coachName,
-        round(ifnull(w1.gameCount, 0), 1) as week1Total,
-        case when ifnull(w1.gameCount, 0) > 3 then 3 else round(ifnull(w1.gameCount, 0), 1) end as week1VAP,
-        round(ifnull(w2.gameCount, 0), 1) as week2Total,
-        case when ifnull(w2.gameCount, 0) > 6 then 6 else round(ifnull(w2.gameCount, 0), 1) end as week2VAP,
-        round(ifnull(w3.gameCount, 0), 1) as week3Total,
-        case when ifnull(w3.gameCount, 0) > 6 then 6 else round(ifnull(w3.gameCount, 0), 1) end as week3VAP,
-        round(ifnull(w4.gameCount, 0), 1) as week4Total,
-        case when ifnull(w4.gameCount, 0) > 3 then 3 else round(ifnull(w4.gameCount, 0), 1) end as week4VAP,
-        round(ifnull(w5.gameCount, 0), 1) as week5Total,
-        case when ifnull(w5.gameCount, 0) > 3 then 3 else round(ifnull(w5.gameCount, 0), 1) end as week5VAP,
-        round(ifnull(w6.gameCount, 0), 1) as week6Total,
-        case when ifnull(w6.gameCount, 0) > 6 then 6 else round(ifnull(w6.gameCount, 0), 1) end as week6VAP,
-        round(ifnull(w7.gameCount, 0), 1) as week7Total,
-        case when ifnull(w7.gameCount, 0) > 3 then 3 else round(ifnull(w7.gameCount, 0), 1) end as week7VAP,
-        round(ifnull(w8.gameCount, 0), 1) as week8Total,
-        case when ifnull(w8.gameCount, 0) > 3 then 3 else round(ifnull(w8.gameCount, 0), 1) end as week8VAP
-    from
-        (
-            select teamId, coachName from team group by 1, 2
-        ) as t
-        left outer join gamesByTeam as w1 on
-            w1.teamId = t.teamId
-            and w1.gameDate = '2017-09-09'
-        left outer join gamesByTeam as w2 on
-            w2.teamId = t.teamId
-            and w2.gameDate = '2017-09-16'
-        left outer join gamesByTeam as w3 on
-            w3.teamId = t.teamId
-            and w3.gameDate = '2017-09-23'
-        left outer join gamesByTeam as w4 on
-            w4.teamId = t.teamId
-            and w4.gameDate = '2017-09-30'
-        left outer join gamesByTeam as w5 on
-            w5.teamId = t.teamId
-            and w5.gameDate = '2017-10-14'
-        left outer join gamesByTeam as w6 on
-            w6.teamId = t.teamId
-            and w6.gameDate = '2017-10-21'
-        left outer join gamesByTeam as w7 on
-            w7.teamId = t.teamId
-            and w7.gameDate = '2017-10-28'
-        left outer join gamesByTeam as w8 on
-            w8.teamId = t.teamId
-            and w8.gameDate = '2017-11-04'
-    ) as data
+    team as t
+    join (select distinct gameDate from gamesByTeam) as gDate
+    left outer join gamesByTeam as g on
+        g.teamId = t.teamId
+        and g.gameDate = gDate.gameDate
+group by
+    1, 2, 3
+order by
+    1, 2, 3;
+
+/* Games By Day for Referee */
+select
+    r.refereeName,
+    gDate.gameDate,
+    ifnull(count, 0) as gamesRefereed,
+    r.teamCount as countOfTeams,
+    round(ifnull(count, 0) / r.teamCount, 1) as gamesPerTeam
+from
+    refereeTeamCount as r
+    join (select distinct gameDate from gamesByTeam) as gDate
+    left outer join game as g on
+        g.refereeName = r.refereeName
+        and g.day = gDate.gameDate
 order by
     1;
